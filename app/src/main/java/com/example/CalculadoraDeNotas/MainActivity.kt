4dba7595f3dbe6e0d1d2e2a82d594aad1879d123
Saotomefirst <jorge.appleseed@gmail.com>
2021-10-10 21:14:49 -0300

    Funcionalidade do botão calcular implementada
    Textos dos botões da tela transformados em linhas do arquivo string.xml
    
    Função de cálculo sem tratameto de erros implementada

--- app/src/main/java/com/example/CalculadoraDeNotas/MainActivity.kt
+++ app/src/main/java/com/example/CalculadoraDeNotas/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.CalculadoraDeNotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btCalcular = calcular
+        val resultado = resultado
+
+        btCalcular.setOnClickListener{
+            val nota1 = Integer.parseInt(nota1.text.toString())
+            val nota2 = Integer.parseInt(nota2.text.toString())
+            val faltas = Integer.parseInt(faltas.text.toString())
+
+            val media = (nota1 + nota2)/2
+
+            if (faltas > 10) {
+                resultado.setText("Aluno Reprovado por Faltas" + "\n" + "Nota Final: $media" + "\n" + "Faltas: $faltas")
+            }
+            else if (media >= 6) {
+                resultado.setText("Aluno Aprovado" + "\n" + "Nota Final: $media" + "\n" + "Faltas: $faltas")
+            }
+            else {
+                resultado.setText("Aluno Reprovado por Nota" + "\n" + "Nota Final: $media" + "\n" + "Faltas: $faltas")
+            }
+
+        }
     }
 }
